5560df56434ab21a1f3a6e573c89cdfd9ad3e04e
iruojefaith <fumsy240@gmail.com>
2022-08-30 01:10:10 +0100

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+
+import Click from './components/click';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+
+      <Click />
+    </div>
+  );
+}
+
+export default App;
---
+++ src/components/click.jsx
@@ -0,0 +1,32 @@
+import React from 'react';
+import { useState } from "react";
+
+
+
+function Click() {
+
+//usestate is use to store data to be rendered in the dom
+const [counter, setCounter] = useState(0);
+
+
+const handleIncrement = (event) => {
+    event.preventDefault();
+    setCounter(increment => increment + 1) ;
+}
+
+const handleDecrement = (event) => {
+    event.preventDefault();
+    setCounter(decrement => decrement - 1);
+}
+
+
+  return (
+    <section>
+        <button onClick={handleIncrement}>+</button>
+        <button className='counterBtn'>{counter}</button>
+        <button onClick={handleDecrement}>-</button>
+    </section>
+  )
+}
+
+export default Click;
